0ef4c3f248030c3de66d486b5dc9dadd5d24f176
Simo Tukiainen <simo.tukiainen@fmi.fi>
2022-12-01 19:03:29 +0200

    Fix versins

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,8 +32,8 @@ extensions = [
 ]
 
 templates_path = ['_templates']
-exclude_patterns = []
-
+exclude_patterns = ["_temp/*"]
+autodoc_member_order = "bysource"
 
 
 # -- Options for HTML output -------------------------------------------------
